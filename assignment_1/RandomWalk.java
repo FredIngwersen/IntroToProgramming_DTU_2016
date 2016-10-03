7df83f91418c2caf2cb7001d7d5931558eb6d1e7
Frederik Ingwersen <frederik.ingwersen@gmail.com>
2016-10-03 16:26:14 +0200

    assignment_1/NumberCheck.java

---
+++ assignment_1/RandomWalk.java
@@ -0,0 +1,28 @@
+package assignment_1;
+
+import java.util.Random;
+import java.math.*;
+
+public class RandomWalk {
+
+	public static void main(String[] args) {
+		
+		runSimulation(10, 6, 4);
+		
+	}
+
+	public static void runSimulation(int n, int t, int s){
+		
+		System.out.println("n=" + n + " t=" + t + " s=" + s);
+		
+		Random grid = new Random();
+		int pointX = grid.nextInt(n);
+		int pointY = grid.nextInt(n);
+		
+		System.out.println("x = " + pointX + " and y = " + pointY);
+		
+		for(int i = 0; i <= t; i++){
+			
+		}
+	}
+}
