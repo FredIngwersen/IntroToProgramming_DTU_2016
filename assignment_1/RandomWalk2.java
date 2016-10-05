508fa32c4a1f0ed4a92dab3b2ca665f203b4bc6c
Frederik Ingwersen <frederik.ingwersen@gmail.com>
2016-10-05 16:18:17 +0200

    Assignment exercise 1 and 2 finished

--- assignment_1/RandomWalk2.java
+++ assignment_1/RandomWalk2.java
@@ -19,8 +19,7 @@ public class RandomWalk2 {
 		int moveParticle_1 = 0;
 		
 		System.out.println("n=" + n + " t=" + t + " s=" + s);
-		System.out.printf("[%d;%d] [%d;%d]\n",
-						  pointX_1, pointY_1, pointX_2, pointY_2 );
+		printPositions(pointX_1, pointY_1, pointX_2, pointY_2);
 		
 		// Starting for loop to run for t moves
 		for(int i = 1; i <= t; i++){
@@ -32,6 +31,62 @@ public class RandomWalk2 {
 			int diffY = Math.abs(pointY_1 - pointY_2);
 			
 			// If x has the largest difference
+			if(diffX > diffY){
+				pointX_2 = getNewPosition2(diffX, s, pointX_1, pointX_2);
+				
+			// If y has the largest difference
+			} else pointY_2 = getNewPosition2(diffY, s, pointY_1, pointY_2); 
+				
+			
+			// Moves Particle1 just as in 1.2a
+			moveParticle_1 = grid.nextInt(s+1) * (grid.nextBoolean() ? 1 : -1);
+			
+			if(randomBool()){
+				if(moveParticle_1 + pointX_1 >= 0 && moveParticle_1 + pointX_1 < n){
+					pointX_1 += moveParticle_1;
+			} else if(moveParticle_1 + pointY_1 >= 0 && moveParticle_1 + pointY_1 < n)
+				pointY_1 += moveParticle_1;
+				
+			}
+			if(pointX_1 == pointX_2 && pointY_1 == pointY_2){
+				printPositions(pointX_1, pointY_1, pointX_2, pointY_2);
+				System.out.println("Crash");
+				break;
+			}
+			printPositions(pointX_1, pointY_1, pointX_2, pointY_2);
+		}
+	}
+	
+	public static boolean randomBool(){
+		return Math.random() < 0.5;
+	}
+	
+	public static void printPositions(int x1, int y1, int x2, int y2){
+		System.out.printf("[%d;%d] [%d;%d]\n", x1, y1, x2, y2 );
+	}
+	
+	// This method creates a new position for the second particle
+	public static int getNewPosition2(int diff, int s, int point1, int point2){
+		// If the difference is larger than or equal to s, move s
+		if(diff >= s){
+			
+			// Determines if the point has to move negative or positive to get closer
+			if(point1 > point2){
+				return (point2 + s);
+			} else return (point2 - s);
+			
+		// If the difference is smaller than s, move the difference
+		} else if(point1 > point2){
+			return (point2 + diff);
+		} else return (point2 - diff);
+		
+	}
+}
+
+
+/*
+
+ 			// If x has the largest difference
 			if(diffX > diffY){
 				
 				// If the difference is larger than or equal to s, move s
@@ -59,28 +114,5 @@ public class RandomWalk2 {
 			} else if(pointY_1 > pointY_2){
 				pointY_2 += diffY;
 			} else pointY_2 -= diffY;
-				
-			// Moves Particle1 just as in 1.2a
-			moveParticle_1 = grid.nextInt(s+1) * (grid.nextBoolean() ? 1 : -1);
-			if(randomBool()){
-				if(moveParticle_1 + pointX_1 >= 0 && moveParticle_1 + pointX_1 < n){
-					pointX_1 += moveParticle_1;
-			} else if(moveParticle_1 + pointY_1 >= 0 && moveParticle_1 + pointY_1 < n)
-				pointY_1 += moveParticle_1;
-				
-			}
-			if(pointX_1 == pointX_2 && pointY_1 == pointY_2){
-				System.out.printf("[%d;%d] [%d;%d]\n",
-						  pointX_1, pointY_1, pointX_2, pointY_2 );
-				System.out.println("Crash");
-				break;
-			}
-			System.out.printf("[%d;%d] [%d;%d]\n",
-					  pointX_1, pointY_1, pointX_2, pointY_2 );
-		}
-	}
-	
-	public static boolean randomBool(){
-		return Math.random() < 0.5;
-	}
-}
+ 
+*/
